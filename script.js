56adaeb72f0db5ce932212b1a57462b62e8786d9
Khai Luca <khaislima@gmail.com>
2025-06-14 21:25:47 -0300

    Primeiro commit do projeto

---
+++ script.js
@@ -0,0 +1,5 @@
+  function adicionarAoCarrinho(nome, preco) {
+    console.log("Produto:", nome);
+    console.log("Preço: R$", preco);
+    alert(`${nome} adicionado ao carrinho por R$ ${preco}`);
+  }
